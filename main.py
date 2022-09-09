7853faca7490578b0199dd7365b842f4a15e1ee1
Ludus <7528518+LudusLight@users.noreply.github.com>
2022-09-09 10:25:51 -0500

    added user command

--- main.py
+++ main.py
@@ -16,5 +16,10 @@ async def accountage(ctx, user : discord.User):
     author = "<@"+str(user.id)+">"
     await ctx.respond("Creation date of "+author+" was <t:"+str(time.mktime(timestamp.timetuple()))[:-2]+":R>"+" on <t:"+str(time.mktime(timestamp.timetuple()))[:-2]+":d>", ephemeral=True)
 
+@bot.user_command(description="Shows the account age of this user", name="Show Account Age")
+async def useraccountage(ctx, user : discord.User):
+    timestamp = user.created_at
+    author = "<@"+str(user.id)+">"
+    await ctx.respond("Creation date of "+author+" was <t:"+str(time.mktime(timestamp.timetuple()))[:-2]+":R>"+" on <t:"+str(time.mktime(timestamp.timetuple()))[:-2]+":d>", ephemeral=True)
 
 bot.run(sys.argv[1])
